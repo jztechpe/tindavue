d7937469d90a1b1fc044246ce3f2664d80f83622
IsaRodz <rodrigueziriarte96@gmail.com>
2020-10-20 13:40:13 -0500

    Fixed Menu trigger

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <h3>Input counter</h3>
 
     <jz-input-counter v-model="quantity"></jz-input-counter>
 
-    <jz-menu :categories="categories" />
+    <div class="mt-5 mb-5">
+      <button @click="isMenuOpen = true" class="button is-primary">
+        Open Menu
+      </button>
+    </div>
+    <jz-menu v-model="isMenuOpen" :categories="categories">
+      <template v-slot:header>
+        <a href="/">Home</a>
+        <a href="#!">Mi cuenta</a>
+        <a href="#!">Mis ordenes</a>
+      </template>
+      <template v-slot:footer>
+        <div class="line">
+          <label>Servicio al cliente</label>
+        </div>
+
+        <a href="#">Cambios y devoluciones</a>
+        <a href="#">Política de privacidad</a>
+        <a href="#">Términos legales</a>
+        <a href="#">Términos y condiciones</a>
+        <a href="#">Contáctenos</a>
+      </template>
+    </jz-menu>
 
     <jz-collapse>
       <template v-slot:title>
@@ -32,26 +54,25 @@
       item-value="value"
       :disabled="true"
     />
-    <JzSelectList v-model="tallaModel2" title="Talla2" :items="['S','XS']" />
+    <JzSelectList v-model="tallaModel2" title="Talla2" :items="['S', 'XS']" />
 
-    <JzModal v-model="showModal" >
-
+    <JzModal v-model="showModal">
       <template #body> Body </template>
     </JzModal>
   </div>
 </template>
 
 <script>
-import '@juntoz/tindacss/scss/tinda.scss';
-import JzInputCounter from './components/JzInputCounter';
-import JzMenu from './components/JzMenu';
-import JzCollapse from './components/JzCollapse';
-import JzSelectFirstImage from './components/JzSelectFirstImage';
-import JzSelectList from './components/JzSelectList';
-import JzModal from './components/JzModal';
+import "@juntoz/tindacss/scss/tinda.scss";
+import JzInputCounter from "./components/JzInputCounter";
+import JzMenu from "./components/JzMenu";
+import JzCollapse from "./components/JzCollapse";
+import JzSelectFirstImage from "./components/JzSelectFirstImage";
+import JzSelectList from "./components/JzSelectList";
+import JzModal from "./components/JzModal";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     JzInputCounter,
     JzMenu,
@@ -62,312 +83,323 @@ export default {
   },
   data() {
     return {
-      showModal:false,
+      showModal: false,
       quantity: 1,
+      isMenuOpen: false,
       categories: [
         {
-          name: 'Supermercados',
+          name: "Supermercados",
           children: [
             {
-              name: 'Alimentos',
-              path: '/categoria/supermercado-alimentos',
-              link: '/link'
+              name: "Alimentos",
+              path: "/categoria/supermercado-alimentos",
+              link: "/link",
             },
             {
-              name: 'Bebidas',
-              path: '/categoria/supermercado-bebidas',
+              name: "Bebidas",
+              path: "/categoria/supermercado-bebidas",
             },
             {
-              name: 'Licores',
-              path: '/categoria/supermercado-licores',
+              name: "Licores",
+              path: "/categoria/supermercado-licores",
             },
           ],
         },
         {
-          name: 'Tecnología y electronica',
+          name: "Tecnología y electronica",
           children: [
             {
-              name: 'Celulares y tablets ',
-              path: '/categoria/celulares',
+              name: "Celulares y tablets ",
+              path: "/categoria/celulares",
             },
             {
-              name: 'Computación',
-              path: '/categoria/bebidas',
+              name: "Computación",
+              path: "/categoria/bebidas",
+              children: [
+                {
+                  name: "Desktop",
+                  path: "/categoria/bebidas",
+                },
+                {
+                  name: "Laptops",
+                  path: "/categoria/bebidas",
+                },
+              ],
             },
             {
-              name: 'TV, audio y video',
-              path: '/categoria/tv',
+              name: "TV, audio y video",
+              path: "/categoria/tv",
             },
             {
-              name: 'Electrodomesticos mayores',
-              path: '/categoria/electrodomesticos',
+              name: "Electrodomesticos mayores",
+              path: "/categoria/electrodomesticos",
             },
             {
-              name: 'Electrodomesticos menores',
-              path: '/categoria/electrodomesticos',
+              name: "Electrodomesticos menores",
+              path: "/categoria/electrodomesticos",
             },
             {
-              name: 'Accesorios',
-              path: '/categoria/accesorios',
+              name: "Accesorios",
+              path: "/categoria/accesorios",
             },
           ],
         },
         {
-          name: 'Hogar',
+          name: "Hogar",
           children: [
             {
-              name: 'Colchones',
-              path: '/categoria/hogar-',
+              name: "Colchones",
+              path: "/categoria/hogar-",
             },
             {
-              name: 'Mobiliario',
-              path: '/categoria/hogar-mobiliario',
+              name: "Mobiliario",
+              path: "/categoria/hogar-mobiliario",
             },
             {
-              name: 'Remodelación',
-              path: '/categoria/hogar-remodelacion',
+              name: "Remodelación",
+              path: "/categoria/hogar-remodelacion",
             },
             {
-              name: 'Accesorios',
-              path: '/categoria/hogar-accesorios',
+              name: "Accesorios",
+              path: "/categoria/hogar-accesorios",
             },
             {
-              name: 'Ferreteria',
-              path: '/categoria/hogar-ferreteria',
+              name: "Ferreteria",
+              path: "/categoria/hogar-ferreteria",
             },
           ],
         },
         {
-          name: 'Deportes',
+          name: "Deportes",
           children: [
             {
-              name: 'Bicicletas',
-              path: '/categoria/deportes-bicicletas',
+              name: "Bicicletas",
+              path: "/categoria/deportes-bicicletas",
             },
             {
-              name: 'Camping',
-              path: '/categoria/deportes-camping',
+              name: "Camping",
+              path: "/categoria/deportes-camping",
             },
             {
-              name: 'Fitness',
-              path: '/categoria/deportes-fitness',
+              name: "Fitness",
+              path: "/categoria/deportes-fitness",
             },
             {
-              name: 'Patinetas',
-              path: '/categoria/deportes-patinetas',
+              name: "Patinetas",
+              path: "/categoria/deportes-patinetas",
             },
             {
-              name: 'Accesorios',
-              path: '/categoria/deportes-accesorios',
+              name: "Accesorios",
+              path: "/categoria/deportes-accesorios",
             },
           ],
         },
         {
-          name: 'Moda',
+          name: "Moda",
           children: [
             {
-              name: 'Hombre/Mujeres',
-              path: '/categoria/moda-hombres-mujeres',
+              name: "Hombre/Mujeres",
+              path: "/categoria/moda-hombres-mujeres",
             },
             {
-              name: 'Deportiva',
-              path: '/categoria/moda-deportiva',
+              name: "Deportiva",
+              path: "/categoria/moda-deportiva",
             },
             {
-              name: 'Calzados',
-              path: '/categoria/moda-calzados',
+              name: "Calzados",
+              path: "/categoria/moda-calzados",
             },
             {
-              name: 'Ropa interior / Home wear',
-              path: '/categoria/moda-ropa-interior',
+              name: "Ropa interior / Home wear",
+              path: "/categoria/moda-ropa-interior",
             },
             {
-              name: 'Swimwear',
-              path: '/categoria/moda-swimwear',
+              name: "Swimwear",
+              path: "/categoria/moda-swimwear",
             },
             {
-              name: 'Accesorios',
-              path: '/categoria/moda-accesorios',
+              name: "Accesorios",
+              path: "/categoria/moda-accesorios",
             },
             {
-              name: 'Relojeria',
-              path: '/categoria/moda-relojeria',
+              name: "Relojeria",
+              path: "/categoria/moda-relojeria",
             },
             {
-              name: 'Joyeria',
-              path: '/categoria/moda-joyeria',
+              name: "Joyeria",
+              path: "/categoria/moda-joyeria",
             },
             {
-              name: 'Opticas',
-              path: '/categoria/moda-opticas',
+              name: "Opticas",
+              path: "/categoria/moda-opticas",
             },
           ],
         },
         {
-          name: 'Libros',
+          name: "Libros",
           children: [
             {
-              name: 'Libros',
-              path: '/categoria/libros-libros',
+              name: "Libros",
+              path: "/categoria/libros-libros",
             },
           ],
         },
         {
-          name: 'Entretenimiento',
+          name: "Entretenimiento",
           children: [
             {
-              name: 'Suscripciones/Revistas',
-              path: '/categoria/entretenimiento-revistas',
+              name: "Suscripciones/Revistas",
+              path: "/categoria/entretenimiento-revistas",
             },
             {
-              name: 'Boletas eventos',
-              path: '/categoria/entretenimiento-boletas-eventos',
+              name: "Boletas eventos",
+              path: "/categoria/entretenimiento-boletas-eventos",
             },
             {
-              name: 'Gift cards',
-              path: '/categoria/entretenimiento-gift-cards',
+              name: "Gift cards",
+              path: "/categoria/entretenimiento-gift-cards",
             },
             {
-              name: 'Servicios/ cursos',
-              path: '/categoria/entretenimiento-servicios-cursos',
+              name: "Servicios/ cursos",
+              path: "/categoria/entretenimiento-servicios-cursos",
             },
             {
-              name: 'Variedades/ Otros',
-              path: '/categoria/entretenimiento-variedades-otros',
+              name: "Variedades/ Otros",
+              path: "/categoria/entretenimiento-variedades-otros",
             },
           ],
         },
         {
-          name: 'Mascotas',
+          name: "Mascotas",
           children: [
             {
-              name: 'Perros',
-              path: '/categoria/mascotas-perros',
+              name: "Perros",
+              path: "/categoria/mascotas-perros",
             },
             {
-              name: 'Gatos',
-              path: '/categoria/mascotas-gatos',
+              name: "Gatos",
+              path: "/categoria/mascotas-gatos",
             },
             {
-              name: 'Otros Accesorios',
-              path: '/categoria/mascotas-otros-accesorios',
+              name: "Otros Accesorios",
+              path: "/categoria/mascotas-otros-accesorios",
             },
           ],
         },
         {
-          name: 'Niños y Bebes',
+          name: "Niños y Bebes",
           children: [
             {
-              name: 'Juegos',
-              path: '/categoria/ninos-y-bebes-juegos',
+              name: "Juegos",
+              path: "/categoria/ninos-y-bebes-juegos",
             },
             {
-              name: 'Juguetes',
-              path: '/categoria/ninos-y-bebes-juguetes',
+              name: "Juguetes",
+              path: "/categoria/ninos-y-bebes-juguetes",
             },
             {
-              name: 'Moda',
-              path: '/categoria/ninos-y-bebes-moda',
+              name: "Moda",
+              path: "/categoria/ninos-y-bebes-moda",
             },
             {
-              name: 'Accesorios',
-              path: '/categoria/ninos-y-bebes-accesorios',
+              name: "Accesorios",
+              path: "/categoria/ninos-y-bebes-accesorios",
             },
           ],
         },
         {
-          name: 'Salud y Belleza',
+          name: "Salud y Belleza",
           children: [
             {
-              name: 'Farmacias',
-              path: '/categoria/salud-belleza-farmacias',
+              name: "Farmacias",
+              path: "/categoria/salud-belleza-farmacias",
             },
             {
-              name: 'Belleza y cuidado personal',
-              path: '/categoria/salud-belleza-belleza-y-cuidado-personal',
+              name: "Belleza y cuidado personal",
+              path: "/categoria/salud-belleza-belleza-y-cuidado-personal",
             },
             {
-              name: 'Perfumeria',
-              path: '/categoria/salud-belleza-perfumeria',
+              name: "Perfumeria",
+              path: "/categoria/salud-belleza-perfumeria",
             },
           ],
         },
         {
-          name: 'Auto y transporte',
+          name: "Auto y transporte",
           children: [
             {
-              name: 'Accesorios',
-              path: '/categoria/auto-y-transportes-accesorios',
+              name: "Accesorios",
+              path: "/categoria/auto-y-transportes-accesorios",
             },
             {
-              name: 'Aceites',
-              path: '/categoria/auto-y-transportes-aceites',
+              name: "Aceites",
+              path: "/categoria/auto-y-transportes-aceites",
             },
             {
-              name: 'Baterias',
-              path: '/categoria/auto-y-transportes-baterias',
+              name: "Baterias",
+              path: "/categoria/auto-y-transportes-baterias",
             },
             {
-              name: 'Llantas',
-              path: '/categoria/auto-y-transportes-llantas',
+              name: "Llantas",
+              path: "/categoria/auto-y-transportes-llantas",
             },
             {
-              name: 'Vehiculos',
-              path: '/categoria/auto-y-transportes-vehiculos',
+              name: "Vehiculos",
+              path: "/categoria/auto-y-transportes-vehiculos",
             },
           ],
         },
         {
-          name: 'Emprendedores',
+          name: "Emprendedores",
           children: [
             {
-              name: 'Accesorios',
-              path: '/categoria/emprendedores-accesorios',
+              name: "Accesorios",
+              path: "/categoria/emprendedores-accesorios",
             },
             {
-              name: 'Arte',
-              path: '/categoria/emprendedores-arte',
+              name: "Arte",
+              path: "/categoria/emprendedores-arte",
             },
             {
-              name: 'Belleza',
-              path: '/categoria/emprendedores-belleza',
+              name: "Belleza",
+              path: "/categoria/emprendedores-belleza",
             },
             {
-              name: 'Hogar y decoración',
-              path: '/categoria/emprendedores-hogar-y-decoracion',
+              name: "Hogar y decoración",
+              path: "/categoria/emprendedores-hogar-y-decoracion",
             },
             {
-              name: 'Joyeria',
-              path: '/categoria/emprendedores-joyeria',
+              name: "Joyeria",
+              path: "/categoria/emprendedores-joyeria",
             },
             {
-              name: 'Mascotas',
-              path: '/categoria/emprendedores-mascotas',
+              name: "Mascotas",
+              path: "/categoria/emprendedores-mascotas",
             },
             {
-              name: 'Moda ',
-              path: '/categoria/emprendedores-moda',
+              name: "Moda ",
+              path: "/categoria/emprendedores-moda",
             },
             {
-              name: 'Niños',
-              path: '/categoria/emprendedores-ninos',
+              name: "Niños",
+              path: "/categoria/emprendedores-ninos",
             },
           ],
         },
         {
-          name: 'Cross Border',
+          name: "Cross Border",
           children: [
             {
-              name: 'Hogar',
-              path: '/categoria/cross-border-hogar',
+              name: "Hogar",
+              path: "/categoria/cross-border-hogar",
             },
             {
-              name: 'Moda',
-              path: '/categoria/cross-border-moda',
+              name: "Moda",
+              path: "/categoria/cross-border-moda",
             },
             {
-              name: 'Tecnologia',
-              path: '/categoria/cross-border-tecnologia',
+              name: "Tecnologia",
+              path: "/categoria/cross-border-tecnologia",
             },
           ],
         },
@@ -375,29 +407,32 @@ export default {
       colorModel: null,
       colorItems: [
         {
-          text: 'azul',
+          text: "azul",
           ImageSmall:
-            'https://jzcatalogstg.blob.core.windows.net/products/b58ac3ec05f44414b674d9244e4538a0/9df301a0e24911eaab06d941f3746bf3/70fc4ab0e32c11eaaee3b983d622c2a4/02cb8ef0e33811eabcf5b1535dd40a53.jpg',
+            "https://jzcatalogstg.blob.core.windows.net/products/b58ac3ec05f44414b674d9244e4538a0/9df301a0e24911eaab06d941f3746bf3/70fc4ab0e32c11eaaee3b983d622c2a4/02cb8ef0e33811eabcf5b1535dd40a53.jpg",
         },
         {
-          text: 'azul 2',
+          text: "azul 2",
           ImageSmall:
-            'https://jzcatalogstg.blob.core.windows.net/products/b58ac3ec05f44414b674d9244e4538a0/9df301a0e24911eaab06d941f3746bf3/70fc4ab0e32c11eaaee3b983d622c2a4/02cb8ef0e33811eabcf5b1535dd40a53.jpg',
+            "https://jzcatalogstg.blob.core.windows.net/products/b58ac3ec05f44414b674d9244e4538a0/9df301a0e24911eaab06d941f3746bf3/70fc4ab0e32c11eaaee3b983d622c2a4/02cb8ef0e33811eabcf5b1535dd40a53.jpg",
         },
         {
-          text: 'Verde',
+          text: "Verde",
           ImageSmall:
-            'https://jzcatalogstg.blob.core.windows.net/products/b58ac3ec05f44414b674d9244e4538a0/9df301a0e24911eaab06d941f3746bf3/70fc4ab0e32c11eaaee3b983d622c2a4/2723e900e33811eabcf5b1535dd40a53.jpg',
+            "https://jzcatalogstg.blob.core.windows.net/products/b58ac3ec05f44414b674d9244e4538a0/9df301a0e24911eaab06d941f3746bf3/70fc4ab0e32c11eaaee3b983d622c2a4/2723e900e33811eabcf5b1535dd40a53.jpg",
         },
         {
-          text: 'Rojo',
+          text: "Rojo",
           ImageSmall:
-            'https://jzcatalogstg.blob.core.windows.net/products/b58ac3ec05f44414b674d9244e4538a0/9df301a0e24911eaab06d941f3746bf3/2c799a60e7c211ea9c9b79761bb77924/5e024460e7c211ea9c9b79761bb77924.png',
+            "https://jzcatalogstg.blob.core.windows.net/products/b58ac3ec05f44414b674d9244e4538a0/9df301a0e24911eaab06d941f3746bf3/2c799a60e7c211ea9c9b79761bb77924/5e024460e7c211ea9c9b79761bb77924.png",
         },
       ],
-      tallaModel: '',
+      tallaModel: "",
       tallaModel2: null,
-      tallaItems: [{"text":"S","value":"S","disabled":true},{"text":"M","value":"M","disabled":false}],
+      tallaItems: [
+        { text: "S", value: "S", disabled: true },
+        { text: "M", value: "M", disabled: false },
+      ],
     };
   },
 };
